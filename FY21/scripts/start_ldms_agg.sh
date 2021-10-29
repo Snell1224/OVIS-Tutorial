21c044400c8d633faeec967f0a87f7b125e6fcff
spwalto <spwalto@sandia.gov>
2021-10-29 12:02:26 -0600

    adding scripts and conf files

---
+++ FY21/scripts/start_ldms_agg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+TOP=/opt/ovis
+export LD_LIBRARY_PATH=$TOP/lib64/:$LD_LIBRARY_PATH
+export LDMSD_PLUGIN_LIBPATH=$TOP/lib64/ovis-ldms
+export PATH=$TOP/sbin:$TOP/bin:$PATH
+export ZAP_LIBPATH=$TOP/lib64/ovis-ldms
+export PYTHONPATH=$TOP/lib/python3.6/site-packages/:$PYTHONPATH
+ 
+ldmsd -x sock:20001 -l /home/user1/ldmscon2021/basic/exercises/ldms/logs/simple_aggd.log -c /home/user1/ldmscon2021/basic/exercises/ldms/conf/E3/agg.conf
